4fc9a24fea43833c7f1fe0cd35287384fd6cf38e
Rachit <rachitarya2001@email.com>
2025-06-11 22:33:53 +0530

    changes : Header compo completed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,58 @@
+import { useAuth } from "@clerk/clerk-react"
+import { Container } from "./container";
+import { cn } from "@/lib/utils"
+import LogoContainer from "./logo-container";
+import NavigationRoutes from "./navigation-routes";
+import { NavLink } from "react-router-dom";
+import ProfileContainer from "./profile-container";
+import ToggleContainer from "./toggle-container";
+
+
 const Header = () => {
+
+    const { userId } = useAuth();
     return (
-        <div>Header</div>
+        <header className={cn("w-full border-b duration-150 transition-all ease-in-out")}>
+            <Container>
+                <div className="flex items-center gap-4 w-full">
+                    {/* {logo section} */}
+                    <LogoContainer />
+
+
+
+                    {/* {navigation section} */}
+
+                    <nav className="hidden md:flex items-center gap-6">
+                        <NavigationRoutes />
+                        {userId && (
+                            <NavLink
+
+                                to={"*/generate"}
+                                className={({ isActive }) => cn(
+                                    "text-base text-neutral-600",
+                                    isActive && "text-neutral-900 font-semibold"
+                                )}
+                            >
+                                Take An Interview
+                            </NavLink>
+
+                        )}
+                    </nav>
+
+
+
+
+                    <div className="ml-auto flex items-center gap-6"></div>
+                    {/* {profile section} */}
+
+                    <ProfileContainer />
+
+                    {/* {mobile toggle section} */}
+                    <ToggleContainer />
+
+                </div>
+            </Container>
+        </header>
     )
 }
 
